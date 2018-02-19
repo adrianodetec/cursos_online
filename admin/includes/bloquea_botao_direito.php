b52fbdf2bf4f195ec6d44d2573d5c3735cc3895c
Adriano Carmo <dinhoninho1207@gmail.com>
2018-02-19 01:39:25 -0300

    ok

--- admin/includes/bloquea_botao_direito.php
+++
@@ -1,7 +0,0 @@
-<script>
-// Desabilitar o botão direito do mouse
-var DADrccolor = "#F1F1F1";
-var DADrcimage = "../images/site_protegido.png";
-</script>
-<script type="text/javascript" src="../js/antirightclick.js"></script>
-
